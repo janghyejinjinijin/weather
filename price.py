de25c9d322f7d684697ab13fa52333b49e49c18a
janghyejinjinjin <hyejin283@naver.com>
2022-06-14 13:11:12 +0900

    Revert "버전 1.0.0000"
    This reverts commit 8b2ae063929ded4e3abad0bd1d57e5994a3b927a.

--- price.py
+++ price.py
@@ -2,39 +2,29 @@ import sys
 # 시스템 불러오기
 from PyQt5 import uic
 # 디자인 만든거 불러와야됨
-from PyQt5.QtCore import QTimer
 from PyQt5.QtWidgets import *
 #큐티위젯 불러오고
 from PyQt5.QtGui import *
 #gui 불러오기
 
-import pyupbit
-# 업비트 파이썬 일
-
 form_class = uic.loadUiType('ui/coinui.ui')[0]
-
 
 class MyWindow(QMainWindow, form_class):
     def __init__(self):
-        super().__init__()
-        self.setupUi(self)
-
-        self.timer = QTimer(self)
-        self.timer.start(1000)
-        self.timer.timeout.connect(self.requestPrice)
-        #self.price_button.clicked.connect(self.requestPrice)
-
-
+    # q메인윈도우 상속받기 그 후 폼클래스를 상속받아야 쓸 수 있는 것임
+        super().__init__()  # 부모 초기화자 호출 안하면 에러
+        self.setUpUi(self)
+        self.price_button.clicked.connect(self.requestPrice)
 
 
     def requestPrice(self):
-        coinPrice = pyupbit.get_current_price(["KRW-BTC","KRW-XRP"])
-        print(coinPrice["KRW-XRP"])
-        self.price_label.setText(f"{coinPrice['KRW-BTC']:,.0f}원")
-#f스트링에 의해 :, 천자리마다 콤마가생김 // .0f를하면 소숫자리수를 없앨 수 있음
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = MyWindow()
-    ex.show()
-    sys.exit(app.exec_())
+
+
+
+
+    if __name__ == '__main__':
+        app = QApplication(sys.argv)
+        ex = MyWindow()
+        ex.show()
+        sys.exit(app.exec_())
